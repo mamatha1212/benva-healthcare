0f879468abf719d5040494d46168817864b9d9c2
Hemachandra-Ponnaganti <hemachandra521@gmail.com>
2026-09-23 15:01:24 +0530

    use standard manifest.json for PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description:
     'Join Benva Healthcare in our mission to provide food, shelter, education, and hope to communities in need. Every act of kindness brings us closer to a better world.',
   keywords: 'healthcare, charity, donation, children, hope, community',
-  manifest: '/manifest.webmanifest',
+  manifest: '/manifest.json',
   appleWebApp: {
     capable: true,
     statusBarStyle: 'default',
