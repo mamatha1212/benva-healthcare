4af739e2e81b80195013f07b4509a59d62a97553
Hemachandra-Ponnaganti <hemachandra521@gmail.com>
2026-09-26 21:55:53 +0530

    Increase logo size in link preview (800x800 square)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: 'https://www.benvahealthcare.in/og-image.jpg',
-        width: 1200,
-        height: 630,
+        width: 800,
+        height: 800,
         type: 'image/jpeg',
         alt: 'Benva Healthcare Logo',
       },
